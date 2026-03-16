9b7f2e2da4cb9f6b00cf3d83d3c254e51a3e39cb
laugiov <laurent.giovannoni@gmail.com>
2026-03-16 17:19:28 +0100

    debug: backtrace in Kernel.php + class_alias check + inline_class_loader=false

--- backend-symfony/src/Kernel.php
+++ backend-symfony/src/Kernel.php
@@ -1,4 +1,17 @@
 <?php
+// TEMP DEBUG: trace who includes this file
+if (\class_exists('App\\Kernel', false)) {
+    \fwrite(\STDERR, "=== App\\Kernel ALREADY DEFINED when Kernel.php included ===\n");
+    foreach (\debug_backtrace(\DEBUG_BACKTRACE_IGNORE_ARGS) as $i => $f) {
+        \fwrite(\STDERR, "#$i " . ($f['file'] ?? '?') . ':' . ($f['line'] ?? '?') . ' ' . ($f['class'] ?? '') . ($f['type'] ?? '') . ($f['function'] ?? '') . "\n");
+    }
+    \fwrite(\STDERR, "=== END ===\n");
+} else {
+    \fwrite(\STDERR, "=== Kernel.php first include ===\n");
+    foreach (\debug_backtrace(\DEBUG_BACKTRACE_IGNORE_ARGS, 5) as $i => $f) {
+        \fwrite(\STDERR, "#$i " . ($f['file'] ?? '?') . ':' . ($f['line'] ?? '?') . ' ' . ($f['class'] ?? '') . ($f['type'] ?? '') . ($f['function'] ?? '') . "\n");
+    }
+}
 
 declare(strict_types=1);
 
