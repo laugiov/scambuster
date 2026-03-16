49210f41da7c1e345c001b2fee0531b461ee9a04
laugiov <laurent.giovannoni@gmail.com>
2026-03-16 17:24:06 +0100

    fix: move debug trace after declare/namespace to fix syntax

--- backend-symfony/src/Kernel.php
+++ backend-symfony/src/Kernel.php
@@ -1,17 +1,4 @@
 <?php
-// TEMP DEBUG: trace who includes this file
-if (\class_exists('App\\Kernel', false)) {
-    \fwrite(\STDERR, "=== App\\Kernel ALREADY DEFINED when Kernel.php included ===\n");
-    foreach (\debug_backtrace(\DEBUG_BACKTRACE_IGNORE_ARGS) as $i => $f) {
-        \fwrite(\STDERR, "#$i " . ($f['file'] ?? '?') . ':' . ($f['line'] ?? '?') . ' ' . ($f['class'] ?? '') . ($f['type'] ?? '') . ($f['function'] ?? '') . "\n");
-    }
-    \fwrite(\STDERR, "=== END ===\n");
-} else {
-    \fwrite(\STDERR, "=== Kernel.php first include ===\n");
-    foreach (\debug_backtrace(\DEBUG_BACKTRACE_IGNORE_ARGS, 5) as $i => $f) {
-        \fwrite(\STDERR, "#$i " . ($f['file'] ?? '?') . ':' . ($f['line'] ?? '?') . ' ' . ($f['class'] ?? '') . ($f['type'] ?? '') . ($f['function'] ?? '') . "\n");
-    }
-}
 
 declare(strict_types=1);
 
@@ -19,6 +6,16 @@ namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+// TEMP DEBUG: trace who includes this file a second time
+if (\class_exists('App\\Kernel', false)) {
+    \fwrite(\STDERR, "=== SECOND INCLUDE of Kernel.php ===\n");
+    foreach (\debug_backtrace(\DEBUG_BACKTRACE_IGNORE_ARGS) as $i => $f) {
+        \fwrite(\STDERR, "#$i " . ($f['file'] ?? '?') . ':' . ($f['line'] ?? '?') . ' ' . ($f['class'] ?? '') . ($f['type'] ?? '') . ($f['function'] ?? '') . "\n");
+    }
+    \fwrite(\STDERR, "=== END ===\n");
+    return;
+}
 
 class Kernel extends BaseKernel
 {
