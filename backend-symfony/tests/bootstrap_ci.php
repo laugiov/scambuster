b6aa8dffe548341857240a776eeacf30e4074ad1
laugiov <laurent.giovannoni@gmail.com>
2026-03-25 10:32:50 +0100

    ci(test): add KERNEL_CLASS to bootstrap_ci.php + use phpunit.ci.xml with DAMA

--- backend-symfony/tests/bootstrap_ci.php
+++ backend-symfony/tests/bootstrap_ci.php
@@ -42,6 +42,7 @@ foreach (spl_autoload_functions() as $loader) {
 
 $_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = $_SERVER['APP_ENV'] ?? 'test';
 $_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] = $_SERVER['APP_DEBUG'] ?? '0';
+$_SERVER['KERNEL_CLASS'] = $_ENV['KERNEL_CLASS'] = 'App\\Kernel';
 
 require __DIR__ . '/../config/bootstrap.php';
 
